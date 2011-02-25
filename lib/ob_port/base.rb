bad43803f89b18838bf9f52622fefe6c697bc291
Steven Hammond <shammond@northpub.com>
2011-02-25 10:18:47 -0500

    Magic inflation works for wiki pages.

--- lib/ob_port/base.rb
+++ lib/ob_port/base.rb
@@ -3,15 +3,35 @@ module MageHand
     attr_accessor :id
     
     def initialize(attributes)
-      attributes ||= {}
+      update_attributes!(attributes)
+    end
+     
+    def update_attributes!(attributes)
+      return unless attributes
       attributes.each do |key, value|
         setter = "#{key}="
         self.send setter, value if self.respond_to?(setter)
       end
     end
     
+    def inflate
+      puts "Inflating wiki page with id: #{self.id}"
+      hash = JSON.parse( MageHand::client.access_token.get(individual_url).body)
+      update_attributes!(hash)
+    end
+    
     def self.model_name
       @_model_name ||= ActiveModel::Name.new(self)
+    end
+    
+    def self.inflate_if_nil(method_name)
+      self.class_eval do
+        alias_method "#{method_name.to_s}_original".to_sym, method_name
+        define_method method_name do
+          inflate if self.send("#{method_name.to_s}_original".to_sym).nil?
+          self.send("#{method_name.to_s}_original".to_sym)
+        end
+      end
     end
   end
 end
--- lib/ob_port/wiki_page.rb
+++ lib/ob_port/wiki_page.rb
@@ -7,21 +7,39 @@ module MageHand
     
     # Private/Friends
     attr_accessor :type, :is_game_master_only, :body, :body_html, :tags
+    inflate_if_nil :body
+    inflate_if_nil :body_html
     
     # TODO Move these to the posts subclass when we have it.
     attr_accessor :post_title, :post_taglines, :post_time
     
     # GM Only fields
     attr_accessor :game_master_info, :game_master_info_markup
+    inflate_if_nil :game_master_info
+    inflate_if_nil :game_master_info_markup
     
     def self.load_wiki_pages(campaign_id)
       wiki_hashes = JSON.parse(
-        MageHand::client.access_token.get("/v1/campaigns/#{campaign_id}/wikis.json?type=Post").body)
+        MageHand::client.access_token.get(collection_url(campaign_id)).body)
       wiki_hashes.map {|hash| WikiPage.new(hash)}
+    end
+    
+    def campaign=(campaign_hash)
+      @campaign ||= Campaign.new(campaign_hash)
     end
     
     def is_post?
       type == 'Post'
+    end
+    
+    private
+    
+    def self.collection_url(campaign_id)
+      "/v1/campaigns/#{campaign_id}/wikis.json"
+    end
+    
+    def individual_url
+      "/v1/campaigns/#{self.campaign.id}/wikis/#{self.id}.json?"
     end
   end
 end
