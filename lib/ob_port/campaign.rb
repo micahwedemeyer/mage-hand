068600883345748e063942a9b37317857ee7bda5
Steven Hammond <shammond@northpub.com>
2011-02-27 12:51:39 -0500

    Magic inflate of campaigns.

--- lib/ob_port/campaign.rb
+++ lib/ob_port/campaign.rb
@@ -2,14 +2,19 @@ module MageHand
   class Campaign < Base
     ROLES = {'game_master' => 'Game Master', 'player' => 'Player'}
     
-    # public methods
-    attr_accessor :name, :campaign_url, :role, :visibility, :game_master_id, :slug
+    # public mini-object methods
+    attr_accessor :name, :campaign_url, :role, :visibility
+    
+    attr_accessor :game_master_id, :slug
+    inflate_if_nil :game_master_id, :slug
     
     # Private/Friends
     attr_accessor :banner_image_url, :play_status, :looking_for_players, :created_at, :updated_at
+    inflate_if_nil :banner_image_url, :play_status, :looking_for_players, :created_at, :updated_at
     
     # Player/GM Only
     attr_accessor :lat, :lng
+    inflate_if_nil :lat, :lng
     
     def looking_for_players?
       looking_for_players
@@ -25,6 +30,12 @@ module MageHand
     
      def posts
        @adventure_logs ||= wiki_pages.select{|page| page.is_post?}
+     end
+     
+     private
+
+     def individual_url
+       "/v1/campaigns/#{self.id}.json"
      end
   end
 end
